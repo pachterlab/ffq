a6ed9b272172155c673301c4430dfb2a7b580aab
Lioscro <phoenixter96@gmail.com>
2021-06-21 14:43:27 -0700

    add MANIFEST.in and update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     keywords='',
     python_requires='>=3.6',
     license='MIT',
-    packages=find_packages(),
+    packages=find_packages(exclude=('tests', 'tests.*')),
     zip_safe=False,
     include_package_data=True,
     install_requires=read('requirements.txt').strip().split('\n'),
