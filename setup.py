999457b56f6affe6d9ccff0dd45352684836c875
Sina Booeshaghi <sbooeshaghi@gmail.com>
2022-08-08 16:48:31 -0700

    updated precommit hooks

--- setup.py
+++ setup.py
@@ -2,45 +2,45 @@ from setuptools import find_packages, setup
 
 
 def read(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.read()
 
 
-long_description = read('README.md')
+long_description = read("README.md")
 
 setup(
-    name='ffq',
-    version='0.2.1',
-    url='https://github.com/pachterlab/ffq',
-    author='Kyung Hoi (Joseph) Min, Sina Booeshaghi, Ángel Gálvez Merchán',
-    author_email='phoenixter96@gmail.com, alisina@caltech.edu, agalvezm@caltech.edu',
-    maintainer='Pachter Lab',
-    maintainer_email='lpachter@caltech.edu',
-    description='A command line tool that makes it easier to find sequencing data from SRA / GEO / ENCODE / ENA / EBI-EMBL / DDBJ / Biosample.',  # noqa
+    name="ffq",
+    version="0.2.1",
+    url="https://github.com/pachterlab/ffq",
+    author="Kyung Hoi (Joseph) Min, Sina Booeshaghi, Ángel Gálvez Merchán",
+    author_email="phoenixter96@gmail.com, alisina@caltech.edu, agalvezm@caltech.edu",
+    maintainer="Pachter Lab",
+    maintainer_email="lpachter@caltech.edu",
+    description="A command line tool that makes it easier to find sequencing data from SRA / GEO / ENCODE / ENA / EBI-EMBL / DDBJ / Biosample.",  # noqa
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    keywords='',
-    python_requires='>=3.6',
-    license='MIT',
-    packages=find_packages(exclude=('tests', 'tests.*')),
+    long_description_content_type="text/markdown",
+    keywords="",
+    python_requires=">=3.6",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "tests.*")),
     zip_safe=False,
     include_package_data=True,
-    install_requires=read('requirements.txt').strip().split('\n'),
+    install_requires=read("requirements.txt").strip().split("\n"),
     entry_points={
-        'console_scripts': ['ffq=ffq.main:cli'],
+        "console_scripts": ["ffq=ffq.main:cli"],
     },
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Topic :: Utilities",
     ],
 )
--- tests/mixins.py
+++ tests/mixins.py
@@ -3,58 +3,47 @@ from unittest import TestCase
 
 
 class TestMixin(TestCase):
-
     @classmethod
     def setUpClass(cls):
         cls.base_dir = os.path.dirname(os.path.abspath(__file__))
-        cls.fixtures_dir = os.path.join(cls.base_dir, 'fixtures')
-        cls.run_path = os.path.join(cls.fixtures_dir, 'SRR8426358.txt')
-        cls.sample_path = os.path.join(cls.fixtures_dir, 'SRS4237519.txt')
-        cls.experiment_path = os.path.join(cls.fixtures_dir, 'SRX3517583.txt')
-        cls.study_path = os.path.join(cls.fixtures_dir, 'SRP178136.txt')
-        cls.fastqs_path = os.path.join(cls.fixtures_dir, 'fastqs.txt')
+        cls.fixtures_dir = os.path.join(cls.base_dir, "fixtures")
+        cls.run_path = os.path.join(cls.fixtures_dir, "SRR8426358.txt")
+        cls.sample_path = os.path.join(cls.fixtures_dir, "SRS4237519.txt")
+        cls.experiment_path = os.path.join(cls.fixtures_dir, "SRX3517583.txt")
+        cls.study_path = os.path.join(cls.fixtures_dir, "SRP178136.txt")
+        cls.fastqs_path = os.path.join(cls.fixtures_dir, "fastqs.txt")
 
         # BAM
-        cls.fastqs2_path = os.path.join(cls.fixtures_dir, 'fastqs_empty.txt')
-        cls.run2_path = os.path.join(cls.fixtures_dir, 'SRR6835844.txt')
-        cls.bam_path = os.path.join(cls.fixtures_dir, 'bam.txt')
-        cls.bam2_path = os.path.join(cls.fixtures_dir, 'bam_empty.txt')
+        cls.fastqs2_path = os.path.join(cls.fixtures_dir, "fastqs_empty.txt")
+        cls.run2_path = os.path.join(cls.fixtures_dir, "SRR6835844.txt")
+        cls.bam_path = os.path.join(cls.fixtures_dir, "bam.txt")
+        cls.bam2_path = os.path.join(cls.fixtures_dir, "bam_empty.txt")
 
         # GEO
-        cls.gse_search_path = os.path.join(
-            cls.fixtures_dir, 'GSE93374_search.txt'
-        )
-        cls.gse_summary_path = os.path.join(
-            cls.fixtures_dir, 'GSE93374_summary.txt'
-        )
+        cls.gse_search_path = os.path.join(cls.fixtures_dir, "GSE93374_search.txt")
+        cls.gse_summary_path = os.path.join(cls.fixtures_dir, "GSE93374_summary.txt")
 
-        cls.gsm_summary_path = os.path.join(
-            cls.fixtures_dir, 'GSM3717978_summary.txt'
-        )
+        cls.gsm_summary_path = os.path.join(cls.fixtures_dir, "GSM3717978_summary.txt")
 
         # ENCODE
-        cls.encode_experiment_path = os.path.join(
-            cls.fixtures_dir, 'ENCSR998WNE.txt'
-        )
+        cls.encode_experiment_path = os.path.join(cls.fixtures_dir, "ENCSR998WNE.txt")
         cls.encode_experiment_output_path = os.path.join(
-            cls.fixtures_dir, 'ENCSR998WNE_output.txt'
+            cls.fixtures_dir, "ENCSR998WNE_output.txt"
         )
-        cls.biosample_path = os.path.join(cls.fixtures_dir, 'ENCBS941ZTJ.txt')
-        cls.donor_path = os.path.join(cls.fixtures_dir, 'ENCDO072AAA.txt')
+        cls.biosample_path = os.path.join(cls.fixtures_dir, "ENCBS941ZTJ.txt")
+        cls.donor_path = os.path.join(cls.fixtures_dir, "ENCDO072AAA.txt")
 
         # Bioproject
 
-        cls.bioproject_path = os.path.join(cls.fixtures_dir, 'CRX118013.txt')
+        cls.bioproject_path = os.path.join(cls.fixtures_dir, "CRX118013.txt")
         # keyed jsons
-        cls.srr_keyed = os.path.join(cls.fixtures_dir, 'SRR5398235_keyed.json')
+        cls.srr_keyed = os.path.join(cls.fixtures_dir, "SRR5398235_keyed.json")
 
         # Alt links
-        cls.alt_links = os.path.join(
-            cls.fixtures_dir, 'SRR6835844_altlinks_new.txt'
-        )
+        cls.alt_links = os.path.join(cls.fixtures_dir, "SRR6835844_altlinks_new.txt")
 
         # SRA
-        cls.srx_xml = os.path.join(cls.fixtures_dir, 'SRX5692097_xml.txt')
+        cls.srx_xml = os.path.join(cls.fixtures_dir, "SRX5692097_xml.txt")
         cls.study_with_run_path = os.path.join(
-            cls.fixtures_dir, 'SRP096361_with_runlist.txt'
+            cls.fixtures_dir, "SRP096361_with_runlist.txt"
         )
