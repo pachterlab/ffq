4bed3242129fec509f6a81bb2729366ef28255ef
Sina Booeshaghi <sbooeshaghi@gmail.com>
2024-03-01 14:49:36 -0800

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ long_description = read("README.md")
 
 setup(
     name="ffq",
-    version="0.3.0",
+    version="0.3.1",
     url="https://github.com/pachterlab/ffq",
     author="Kyung Hoi (Joseph) Min, Sina Booeshaghi, Ángel Gálvez Merchán",
     author_email="phoenixter96@gmail.com, alisina@caltech.edu, agalvezm@caltech.edu",
